66c9228d95e0f20df9cc815ba72cc97ba26e1a2e
maknyk <maksnyk2002@gmail.com>
2022-12-12 17:34:22 +0100

    v1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Area triangulo\n2. Area círculo\n3. Area del cono\n4. Area del cilindro");
+        int opción=sc.nextInt();
+        switch (opción){
+            case 1:
+                System.out.println("Introduce la base del triangulo");
+                int base=sc.nextInt();
+                System.out.println("Introduce la altura del triangulo");
+                int altura=sc.nextInt();
+                System.out.println("El área del triangulo es: "+(base*altura/2));
+        }
+
+
+
+
+    }
+}
